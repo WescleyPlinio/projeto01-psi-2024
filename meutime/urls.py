f2f97600b7d01a164342ea448c2b6f6ac0ddf28a
Wescley <wescleygaldino06@gmail.com>
2024-08-09 10:23:18 -0300

    Finalizando quase tudo, menos as fotos dos cria

--- meutime/urls.py
+++ meutime/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name="index"),
     path('index.html', views.index, name="index"),
-    path('atletas.html', views.atletas, name="atletas")
+    path('atletas.html', views.atletas, name="atletas"),
     path('sobre.html', views.sobre, name="sobre")
 ]
--- meutime/views.py
+++ meutime/views.py
@@ -5,22 +5,22 @@ def index(request):
 
 def atletas(request):
     lista_jogadoras = [
-        {"nome": "Ana Cristina Souza", "idade": 20, "local_nascimento": "Santo André, SP", "posicao": "Ponteira"},
-        {"nome": "Gabi Guimarães", "idade": 29, "local_nascimento": "Belo Horizonte, MG", "posicao": "Ponteira"},
-        {"nome": "Julia Bergmann", "idade": 23, "local_nascimento": "Munich, Alemanha", "posicao": "Ponteira"},
-        {"nome": "Carol Gattaz", "idade": 42, "local_nascimento": "São José do Rio Preto, SP", "posicao": "Central"},
-        {"nome": "Diana Duarte", "idade": 27, "local_nascimento": "Maringá, PR", "posicao": "Central"},
-        {"nome": "Thaisa Menezes", "idade": 37, "local_nascimento": "Rio de Janeiro, RJ", "posicao": "Central"},
-        {"nome": "Tainara Santos", "idade": 23, "local_nascimento": "Volta Redonda, RJ", "posicao": "Oposta"},
-        {"nome": "Rosamaria Montibeller", "idade": 29, "local_nascimento": "Nova Trento, SC", "posicao": "Oposta"},
-        {"nome": "Nyeme Costa", "idade": 25, "local_nascimento": "Miracema, RJ", "posicao": "Líbero"},
-        {"nome": "Roberta Ratzke", "idade": 34, "local_nascimento": "Curitiba, PR", "posicao": "Levantadora"},
-        {"nome": "Macris Carneiro", "idade": 34, "local_nascimento": "Itajubá, MG", "posicao": "Levantadora"},
+        {"nome": "Ana Cristina Souza", "idade": 20, "local_nascimento": "Santo André, SP", "posicao": "Ponteira", "foto":"imgs/Anacris.jpg"},
+        {"nome": "Gabi Guimarães", "idade": 29, "local_nascimento": "Belo Horizonte, MG", "posicao": "Ponteira", "foto":"imgs/Gabi.jpg"},
+        {"nome": "Julia Bergmann", "idade": 23, "local_nascimento": "Munich, Alemanha", "posicao": "Ponteira", "foto":"imgs/juliab.jpg"},
+        {"nome": "Ana Carolina", "idade": 33, "local_nascimento": "Belo Horizonte, MG", "posicao": "Central", "foto":"imgs/Anac.jpg"},
+        {"nome": "Carol Gattaz", "idade": 42, "local_nascimento": "São José do Rio Preto, SP", "posicao": "Central", "foto":"imgs/Carol.jpg"},
+        {"nome": "Diana Duarte", "idade": 27, "local_nascimento": "Maringá, PR", "posicao": "Central", "foto":"imgs/Diana.jpg"},
+        {"nome": "Thaisa Menezes", "idade": 37, "local_nascimento": "Rio de Janeiro, RJ", "posicao": "Central", "foto":"imgs/Thaisa.jpg"},
+        {"nome": "Tainara Santos", "idade": 23, "local_nascimento": "Volta Redonda, RJ", "posicao": "Oposta", "foto":"imgs/Tainara.jpg"},
+        {"nome": "Rosamaria Montibeller", "idade": 29, "local_nascimento": "Nova Trento, SC", "posicao": "Oposta", "foto":"imgs/Rosam.jpg"},
+        {"nome": "Nyeme Costa", "idade": 25, "local_nascimento": "Miracema, RJ", "posicao": "Líbero", "foto":"imgs/Nyeme.jpg"},
+        {"nome": "Roberta Ratzke", "idade": 34, "local_nascimento": "Curitiba, PR", "posicao": "Levantadora", "foto":"imgs/Roberta.jpg"},
 
     ]
     
     context = {
-        "jogadoras": lista_jogadoras,
+        "lista_jogadoras": lista_jogadoras,
     }
     return render(request, "atletas.html",context)
 
