47be9322bb87eb8a597524bed6846320ee267a60
Wescley <wescleygaldino06@gmail.com>
2024-08-09 09:21:51 -0300

    configurando urls e finalizando atletas

--- meutime/urls.py
+++ meutime/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('index.html', views.index, name="index"),
     path('atletas.html', views.atletas, name="atletas")
+    path('sobre.html', views.sobre, name="sobre")
 ]
